82b180e2666cacdbe8e9a2a56e218af236ab7560
Tim Schindler <tim.schindler@adcloud.com>
2012-10-12 20:44:02 +0200

    removed test server script

--- test.js
+++
@@ -1,9 +0,0 @@
-
-  var server = require('http').createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end("pong");
-  });
-
-  server.listen("8124", function () {
-  });
-
